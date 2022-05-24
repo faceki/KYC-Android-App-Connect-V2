5caeb1ecae511d1f852d50f8668de13a1bf53f6b
patelparthc <patel.parth.c@gmail.com>
2022-05-24 15:28:24 +0530

    Added Scan by gallary feature

--- app/src/main/java/com/palm/connectapp/FirstFragment.kt
+++ app/src/main/java/com/palm/connectapp/FirstFragment.kt
@@ -1,23 +1,44 @@
 package com.palm.connectapp
 
-import android.os.Build
-import android.os.Bundle
+import android.Manifest
+import android.R.attr.bitmap
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.*
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.budiyev.android.codescanner.*
 import com.faceki.connectapp.R
 import com.faceki.connectapp.databinding.FragmentFirstBinding
 import com.facekikycverification.startup.FacekiVerification
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
 import com.palm.connectapp.util.FaceKiPreferences
 import com.palm.connectapp.util.Utility
+import java.io.FileNotFoundException
+import java.util.*
+import android.os.AsyncTask
+import android.app.ProgressDialog
+
+
+
+
+
+
 
 
 /**
@@ -50,6 +71,9 @@ class FirstFragment : Fragment() {
         binding.btnReset.setOnClickListener{
            reset()
         }
+        binding.btnGallery.setOnClickListener{
+            openGallery()
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             activity?.window?.statusBarColor = requireView().context!!.getColor(R.color.white)
         } // set status background white
@@ -62,7 +86,15 @@ class FirstFragment : Fragment() {
         callFaceKiVerification()
 
     }
+    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+    val granted = permissions.entries.all {
+            it.value == true
+        }
+        if (granted) {
+            openGallery()
+        }
 
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -202,5 +234,131 @@ class FirstFragment : Fragment() {
     fun goToSetting(){
         findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
     }
+
+
+    /***
+     * Open Gallery
+     */
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+                 val data: Intent? = result.data
+
+                val uri = data?.data
+
+                if (uri != null) {
+                    val mainLooper = Looper.getMainLooper()
+
+                   binding.progressBarCyclic.visibility=View.VISIBLE
+
+                    Thread(Runnable {
+                        val bitmap = uriToBitmap(uri)
+                        try {
+
+                            //   bitmap = getResizedBitmap(bitmap,100)!!
+                            val width = bitmap.width
+                            val height = bitmap.height
+                            val pixels = IntArray(width * height)
+                            bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
+                            bitmap.recycle()
+
+                            val source = RGBLuminanceSource(width, height, pixels)
+                            val bBitmap = BinaryBitmap(HybridBinarizer(source))
+                            val reader = MultiFormatReader()
+                            try {
+                                val result: Result = reader.decode(bBitmap)
+                                Handler(mainLooper).post {
+                                    requireActivity().runOnUiThread {
+                                        binding.progressBarCyclic.visibility=View.GONE
+                                        try {
+                                        val decryptValue =   Utility.decrypt(result.getText(),"ABCDEF1234567890")
+                                        goToVerification(decryptValue)
+                                        } catch (e: Exception) {
+                                                    Toast.makeText(
+                                                        requireActivity(),
+                                                        "Invalid QR code",
+                                                        Toast.LENGTH_LONG
+                                                    ).show()
+                                        }
+                                    }
+                                }
+
+                            } catch (e: Exception) {
+                                Handler(mainLooper).post {
+                                    requireActivity().runOnUiThread {
+                                        binding.progressBarCyclic.visibility=View.GONE
+                                        Toast.makeText(requireActivity(),"Invalid QR code",Toast.LENGTH_LONG).show()
+                                    }
+                                }
+
+                                //e.printStackTrace()
+                            }
+                        } catch (e: FileNotFoundException) {
+                            Handler(mainLooper).post {
+                                requireActivity().runOnUiThread {
+                                    binding.progressBarCyclic.visibility=View.GONE
+                                    Toast.makeText(requireActivity(),"Invalid QR code",Toast.LENGTH_LONG).show()
+                                }
+                            }
+                        }
+
+                    }).start()
+                }
+
+        }
+    }
+
+
+    private fun openGallery(){
+
+        val PERMISSIONS = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+        )
+          if(hasPermissions(requireContext(), permissions =PERMISSIONS ))     {
+
+            val intent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI
+            )
+            intent.type = "image/*"
+              intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+
+//            intent.putExtra("crop", "true")
+//            intent.putExtra("scale", true)
+//            intent.putExtra("aspectX", 16)
+//            intent.putExtra("aspectY", 9)
+            resultLauncher.launch(Intent.createChooser(intent, "Select Picture"))
+
+
+
+          } else {
+
+              checkPermission.launch(PERMISSIONS)
+        }
+    }
+
+    // util method
+    private fun hasPermissions(context: Context, permissions: Array<String>): Boolean = permissions.all {
+        ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    private fun uriToBitmap(uri: Uri): Bitmap {
+        if(Build.VERSION.SDK_INT < 28) {
+            return MediaStore.Images.Media.getBitmap(
+                requireActivity().contentResolver,
+                uri
+            )
+        } else {
+            val source = ImageDecoder.createSource(requireActivity().contentResolver, uri)
+            return ImageDecoder.decodeBitmap(
+                source,
+                ImageDecoder.OnHeaderDecodedListener { decoder, info, source ->
+                    decoder.allocator = ImageDecoder.ALLOCATOR_SOFTWARE
+                    decoder.isMutableRequired = true
+                })
+        }
+    }
+
 
 }
